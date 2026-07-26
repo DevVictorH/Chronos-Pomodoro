2f0614447a6aea43f921362b927cf1cacb7c37a2
Victor Hugo <victorvilto@gmail.com>
2026-07-26 19:48:09 -0300

    Criando pagina de sobre e not found

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,22 @@
+import { Container } from "../../components/Container";
+import { CountDown } from "../../components/CountDown";
+import { MainForm } from "../../components/MainForm";
+import { MainTemplates } from "../../templates/MainTemplate";
+
+
+export function Home() {
+    return (
+        <MainTemplates>
+
+            <Container>
+                <CountDown />
+            </Container>
+
+            <Container>
+                <MainForm />
+            </Container>
+
+        </MainTemplates>
+    )
+
+}
---
+++ src/templates/MainTemplate/index.tsx
@@ -0,0 +1,31 @@
+import { Logo } from "../../components/Logo";
+import { Footer } from "../../components/Footer";
+import { Menu } from "../../components/Menu";
+import { Container } from "../../components/Container";
+
+type MainTemplatesProps = {
+  children: React.ReactNode;
+};
+
+export function MainTemplates({ children }: MainTemplatesProps) {
+
+  return (
+    <>
+
+      <Container>
+        <Logo />
+      </Container>
+
+      <Container>
+        <Menu />
+      </Container>
+
+      {children}
+
+      <Container> <Footer /> </Container>
+
+
+    </>
+  )
+
+}
