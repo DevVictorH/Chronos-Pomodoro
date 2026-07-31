d3c8abf21468521d687fbc8df1bfc82ecafcb9d9
Victor Hugo <victorvilto@gmail.com>
2026-07-30 21:36:29 -0300

    Criando contexto para aplicacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import './styles/theme.css'
 import './styles/global.css'
 import { Home } from './pages/Home'
+import { TaskContextProvider } from './contexts/TaskContext';
+
 
 export function App() {
+
   return (
-    <Home />
+    <TaskContextProvider>
+      <Home />
+    </TaskContextProvider>
   )
 }
---
+++ src/models/TaskStateModel.ts
@@ -0,0 +1,14 @@
+import type { TaskModel } from "./TaskModel"
+
+export type TaskStateModel = {
+    tasks: TaskModel[]
+    secondsRemaining: number
+    formattedSecondsRemaining: string
+    activeTask: TaskModel | null
+    currentCycle: number
+    config: {
+        work: number
+        shortBreakDuration: number
+        longBreakDuration: number
+    }
+}
